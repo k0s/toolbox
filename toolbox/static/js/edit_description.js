f3a4e4c759f1c35fe136540640988192432eb811
Jeff Hammel <jhammel@mozilla.com>
2011-02-28 18:12:04 -0800

    move the ajax editing front end to its own file

---
+++ toolbox/static/js/edit_description.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+        $('div.project').each(function(){
+                var project = $(this).attr('id'); 
+                $(this).find('p.description').editable(project, {
+                        'type': 'textarea',
+                            'rows': 7,
+                            'indicator': '<img src="/img/indicator.gif"/>',
+                            'onblur': 'submit',
+                            'name': 'description',
+                            });
+            });
+    });
