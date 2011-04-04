71269c4f124ca3b7450ea170733398b7950f251d
Jeff Hammel <jhammel@mozilla.com>
2011-04-04 15:23:01 -0700

    remove obselescent focus event

--- toolbox/static/js/project.js
+++ toolbox/static/js/project.js
@@ -67,7 +67,6 @@ $(document).ready(function(){
                                 $(this).val('');
                             }
                         });
-                    //                    $(input).focus();
                 }
 
                 // add an add fields button
