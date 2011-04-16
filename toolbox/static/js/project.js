b6450651492b6e7f4e23c196af9c6766674cfe5f
Jeff Hammel <jhammel@mozilla.com>
2011-04-15 17:47:10 -0700

    more attempts at the same

--- toolbox/static/js/project.js
+++ toolbox/static/js/project.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
                             'tooltip': 'click to edit description'
                             });
 
-                // make the name and url editable [TODO]
+                // make the name and url editable
                 function nameHover(eventObject) {
                     var img = $('<img class="UEB" src="/img/UEB16.png"/>');
                     $(this).append(img);
