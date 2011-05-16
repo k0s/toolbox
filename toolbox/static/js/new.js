16c545486ff5898816b289a8fd88579fa8d14af7
Jeff Hammel <jhammel@mozilla.com>
2011-05-16 11:19:41 -0700

    do new tool form in JS

--- toolbox/static/js/new.js
+++ toolbox/static/js/new.js
@@ -1,6 +1,45 @@
 // javascript for the new tool view
 
 $(document).ready(function(){
+
+    // prepare the form
+    var form = $('<form id="new" method="post"></form>');
+    var table = $('<table></table>');
+
+    function addRow(fieldName, rhs) {
+        $(table).append('<tr><td class="field-name" for="' + fieldName + '">' + fieldName + '</td><td>' + rhs + '</td></tr>')
+    }
+    function addTextInput(fieldName, isFieldInput) {
+        var input = '';
+        if (isFieldInput) {
+            input = '<input type="text" class="field-input" name="' + fieldName + '"/>';
+        } else {
+            input = '<input type="text" name="' + fieldName + '"/>';
+        }
+        addRow(fieldName, input);
+    }
+
+    // insert mandatory data: name, description, url
+    addTextInput('name', false);
+    addRow('description', '<textarea name="description"></textarea>');
+    addTextInput('url', false);
+
+    // find other fields
+    var fields = [];
+    $('.by-field').each(function() {
+        fields.push($(this).text());
+    });
+    for (var i in fields) {
+        addTextInput(fields[i], true);
+    }
+
+    // add the form to the DOM
+    $(form).append(table);
+    $(form).append('<input id="submit-new-tool" class="submit button" type="submit" value="Add it!"/>');
+    $('#new-container').append(form);
+    // TODO: add submit guards
+
+    // add autocomplete
     $('input.field-input').each(function(index) {
         var field = $(this).attr('name');
         $(this).tokenInput("tags?format=json&field=" + field, {
@@ -10,5 +49,7 @@ $(document).ready(function(){
             hintText: false
         });
     });
+
+    // focus the first field: name
     $("input[name=name]").focus();
 });
