896b4c91c231481496dfa1cdd07e6eb48001a8b6
Heather Arthur <fayearthur@gmail.com>
2011-05-27 14:40:14 -0700

    don't linkify period after a url

--- toolbox/static/js/jquery.autolink.js
+++ toolbox/static/js/jquery.autolink.js
@@ -7,7 +7,7 @@ jQuery.fn.highlight = function (text, o) {
 
 jQuery.fn.autolink = function () {
 	return this.each( function(){
-		var re = /((http|https|ftp):\/\/[\w?=&.\/-;#~%-]+(?![\w\s?&.\/;#~%"=-]*>))/g;
+		var re = /((http|https|ftp):\/\/[\w?=&.\/-;#~%-]+(?![\w\s?&.\/;#~%"=-]*>)[^.])/g;
 		$(this).html( $(this).html().replace(re, '<a href="$1">$1</a> ') );
 	});
 }
